09e39df97553c24f6f67c92c69c0f96022de9377
Liu Hua <a224412142009@hotmail.com>
2021-08-15 22:44:15 +0800

    20210815

--- coding-interview-university/algorithm/binary_search/main.go
+++ coding-interview-university/algorithm/binary_search/main.go
@@ -6,18 +6,20 @@ import (
 )
 
 func main() {
-	v := BinarySearch(75)
-	log.Println(v)
+	times := BinarySearch(75)
+	log.Println(times)
 }
 
-// BinarySearch 搜尋已排序過的數列
-func BinarySearch(target int) int {
+// 補充：線性搜尋法(Linear search)
+// 就是從頭開始找，時間複雜度O(n)
+
+// BinarySearch 二分搜尋法-搜尋已排序過的數列
+func BinarySearch(target int) (times int) {
 	arr := make([]int, 100)
 	for i := 0; i < 100; i++ {
 		arr[i] = i
 	}
 
-	times := 0
 	LeftSide := 0
 	RightSide := len(arr) - 1
 
